c44f60b06afadc5e744f04c6422894af3aa3f725
eleacarton <eleacarton@me.com>
2022-05-06 16:14:37 +0200

    avancement

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # TestsUnitaires
+
+> git clone https://github.com/elea77/projet-orm-api.git
+
+> pip install -r requirements.txt
+
+
+## Windows
+
+### Créer l'environnement
+> py -3 -m venv venv
+### Activer l'environnement
+> venv\Scripts\activate
+### Lancer le serveur
+> py -3 app.py
+
+## Mac OS
+
+### Créer l'environnement
+> python3 -m venv venv
+### Activer l'environnement
+> . venv/bin/activate
+### Lancer le serveur
+> python3 app.py
--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, request
 from bson.objectid import ObjectId
-import db
-
+import db, json
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,24 +14,37 @@ class HelloWorld(Resource):
         element["_id"] = str(element["_id"])
         return {'get' : element }
     
-    def put(self, id, title, description, duration):
-        element = movie.find_one_and_update({"_id": ObjectId(id)}, {"title": title, "description": description, "duration": duration})
-        element["_id"] = str(element["_id"])
-        return {'put' : element}
+    def put(self, id):
+        informations =  json.loads(request.data)
+        movie.update_one({ '_id': ObjectId(id) }, {'$set': {'title': informations['title'], 'description': informations['description'], 'duration': informations['duration'] }})
+        return {'id': str(id)}
 
     def delete(self, id):
         element = movie.delete_one({"_id": ObjectId(id)})
         # element["_id"] = str(element["_id"])
         return {'delete' :str(id)}
 
-    def post(self, title, description, duration):
-        movie.insert_one( { "title" : title, "description": description, "duration": duration } )
-        return {'post' :str(title)}
+    def post(self):
+        informations =  json.loads(request.data)
+        movie.insert_one( {'title': informations['title'], 'description': informations['description'], 'duration': informations['duration'] } )
+        return {'post' : 'sucess'}
 
-api.add_resource(HelloWorld, '/<string:id>')
-# api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/', '/<string:id>')
 
 # request
+
+# get
+# requests.get('http://127.0.0.1:5000/62751ef9cc83db8478d76f4c').json()
+
+# put
+# requests.put('http://127.0.0.1:5000/62751ef9cc83db8478d76f4c', json = {"title": "The Batman","description":"bijour","duration":"2h40"}).json()
+
+# post
+# requests.post('http://127.0.0.1:5000/', json = {"title": "TEST","description":"bijour","duration":"2h40"})
+
+# delete
+# requests.delete('http://127.0.0.1:5000/62751ef9cc83db8478d76f4c')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
